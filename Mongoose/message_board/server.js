211d117ddc9c14c686f16078c74d6cdb447eae8c
Nicholas Rampton <nick.rampton@gmail.com>
2017-12-01 10:18:04 -0500

    Just a mid-day commit.

--- Mongoose/message_board/server.js
+++ Mongoose/message_board/server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));       //app.use to import/setu
 app.use(express.static(__dirname + '/static'));
 
 app.set('views', __dirname + '/views');         //app.set to create/modify settings in the app config object
-app.set('views engine', 'ejs');
+app.set('view engine', 'ejs');
 
 mongoose.connect('mongodb://localhost/message_board');      //connecting to database
 
@@ -18,13 +18,13 @@ var MessageSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 4},
   content: { type: String, required: true},
   comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}],
-}, {timesptamps: true})
+}, {timesptamps: true});
 
 var CommentSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 4},
   content: { type: String, required: true},
   _message: {type: Schema.Types.ObjectId, ref: 'Message'},
-}, { timestamps: true})
+}, { timestamps: true});
 
 mongoose.model('Message', MessageSchema);       //setting then getting the models according to the schemata we made
 mongoose.model('Comment', CommentSchema);
@@ -32,14 +32,29 @@ const Message = mongoose.model('Message');
 const Comment = mongoose.model('Comment');
 
 
-app.get('/', (req, res) => {              //Main page route, renders index.ejs
-  Message.find({}).populate('Comments').exec((err, posts) => {
+app.get('/', (req, res) => {                                        //Main page route, renders index.ejs
+  Message.find({}).populate('Comments').exec((err, messages) => {
   if (err) {
     console.log('Something went wrong with the database query.');
     return res.redirect('/oops');
   }  
-  return res.render('index', {posts: posts});
+  return res.render('index', {messages: messages});
   });
+})
+
+app.get('/oops', (req, res) => {            //Route in case the main db query fails.
+  return res.render('oops');
+})
+
+app.post('/new_message', (req, res) => {
+  var body = req.body;
+  var new_message = new Message(body);
+  new_message.save((err) => {
+    if (err) {
+      console.log("Couldn't post that message.");
+      
+    }
+  })
 })
 
 
