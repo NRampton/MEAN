06efe86d1a967d51ec84c9d940d7e9bb5a44b102
Nicholas Rampton <nick.rampton@gmail.com>
2017-12-01 17:57:21 -0500

    Started login and registration assignment for Mongoose. Day 33.

--- 2Mongoose/1955API/server.js
+++ 2Mongoose/1955API/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-var app = express();
+const app = express();
 
 app.use(bodyParser.json());
 
 require('./server/config/mongoose.js');
 
 var routes_setter = require('./server/config/routes');
-routes_setter();
+routes_setter(app);
 
 app.listen(8000, () => {
   console.log("Online.");
