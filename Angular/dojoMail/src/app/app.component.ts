36f02ee72672dc4f493bb3957e63c6dc553cd8e4
Nicholas Rampton <nick.rampton@gmail.com>
2017-12-05 17:12:45 -0500

    Finished DojoMail assignment in Angular.

--- Angular/dojoMail/src/app/app.component.ts
+++ Angular/dojoMail/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+class Email {
+  email: string;
+  importance: boolean;
+  subject: string;
+  content: string;
+  
+  constructor(email: string, importance: boolean, subject: string, content: string){
+    this.email = email;
+    this.importance = importance;
+    this.subject = subject;
+    this.content = content;
+  }
+}
+var emailList = [];
+var addEmail = function(email: string, importance: boolean, subject: string, content: string) {
+  let newEmail = new Email(email, importance, subject, content);
+  emailList.push(newEmail);
+}
+addEmail('bill@gates.com', true, 'New Windows', 'Windows XI will launch in year 2100.');
+addEmail('ada@lovelace.com', true, 'Programming', 'Enchantress of Numbers');
+addEmail('john@carmac.com', false, 'Updated Algo', 'New algorithm for shadow volumes');
+addEmail('gabe@newelcom', false, "HL3!", 'Just kidding...');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  emailList = emailList;
 }
