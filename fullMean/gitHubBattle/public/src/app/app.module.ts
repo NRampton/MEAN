130aebd9a3433cfa0b1da09b94ef676caabf2cfd
Nicholas Rampton <nick.rampton@gmail.com>
2017-12-14 07:44:08 -0500

    Day 42. Cutting my losses on a failed attempt at GitHub Battle. Replacing complex routing with *ngIf statements.

--- fullMean/gitHubBattle/public/src/app/app.module.ts
+++ fullMean/gitHubBattle/public/src/app/app.module.ts
@@ -1,40 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { BattleComponent } from './battle/battle.component';
-import { RankingComponent } from './ranking/ranking.component';
-import { FormComponent } from './battle/form/form.component';
-import { DisplayUserComponent } from './battle/display-user/display-user.component';
-import { ResultsComponent } from './battle/results/results.component';
-
-import { UserService } from './user.service';
-import { LeftComponent } from './battle/left/left.component';
-import { RightComponent } from './battle/right/right.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BattleComponent,
-    RankingComponent,
-    FormComponent,
-    DisplayUserComponent,
-    ResultsComponent,
-    LeftComponent,
-    RightComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpModule,
-    FormsModule
+    BrowserModule
   ],
-  providers: [UserService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- fullMean/gitHubBattle/public/src/app/user.ts
+++
@@ -1,8 +0,0 @@
-export class User {
-  _id: string;
-  name: string;
-  score: number;
-  image: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
